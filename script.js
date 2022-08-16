d2f0707ea89b0c8f49b127b432d2572efbb1f487
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-08-16 17:21:57 +0530

    Variable types

---
+++ script.js
@@ -0,0 +1,11 @@
+////////////////////////
+var a = 10; // defining the variable
+a = 11;
+console.log(a);
+
+let firstName = "Ranjith"; // we are using it here for the changing the values in further
+firstName = "Rajesh"; // we are changing the firstName value here
+console.log(firstName);
+
+const x = 12; // It is the constant type of declaring
+console.log(x);
